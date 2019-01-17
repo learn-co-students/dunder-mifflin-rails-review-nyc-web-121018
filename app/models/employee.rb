a36cb82823013889179c205a42b1ec92b3b578ca
Daniel Fries <daniel.w.fries@gmail.com>
2019-01-17 11:34:48 -0500

    pairing lab complete w/ @sharicodes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,20 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+  validates :title, uniqueness: true
+  validate :has_alias
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def has_alias
+    if self.alias != "none"
+      Employee.all.each do |e|
+        if e.alias.match(self.alias)
+          errors.add(:alias, "Dwight says get a unique alias.")
+        end
+      end
+    end
+  end
+
 end
