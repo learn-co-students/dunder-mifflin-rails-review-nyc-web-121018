5b063083378e5a5ca2020ceda9c51c68ea04726d
diaaanek <dkorongy@gmail.com>
2019-01-17 13:23:50 -0500

    commitng

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+
+
+
+def index
+    @dogs = Dog.all
+    @employees = Employee.all
 end
+
+def show
+    @dog = Dog.find(params[:id])
+end
+
+end # end Dogs controller
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,53 @@
 class EmployeesController < ApplicationController
+
+  def index
+      @employees = Employee.all
+          @dogs = Dog.all
+  end
+
+  def show
+      @employee = Employee.find(params[:id])
+  end
+  def new
+    @employee = Employee.new
+
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      @employee.save
+#right
+    redirect_to employee_path(@employee)
+#wrong
+  else
+    redirect_to new_employee_path
+  end
+  end
+
+  def edit
+          @employee = Employee.find(employee_params[:id])
+  end
+
+
+def update
+  @employee = Employee.find(params[:id])
+  @employee.update(employee_params)
+  # validates
+  if @employee.valid?
+    redirect_to employee_path(@employee)
+  else
+    render :edit
+  end
+
 end
+
+private
+
+
+def employee_params
+
+params.require(:employee).permit(:first_name, :last_name,:alias, :title, :office, :dog_id)
+end
+
+end # end employees controller
